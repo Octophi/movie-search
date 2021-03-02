972209f9c0f6f1b57e16863d38a17085f04ac065
octophi <16705282+Octophi@users.noreply.github.com>
2021-03-02 16:35:06 -0500

    cleaned up searchbar, display of results with flexbox

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+// Basic API Setup
+const apiKey = "4b547eeea89c2c56ed31012705fbf0c6";
+const apiConfigRequest = `https://api.themoviedb.org/3/configuration?api_key=${apiKey}`;
+
+// Querying for basic config info, base_url and size used for finding images
+let secure_base_url, size;
+fetch(apiConfigRequest)
+  .then((response) => response.json())
+  .then((data) => {
+    secure_base_url = data.images.secure_base_url;
+    console.log(secure_base_url);
+    size = data.images.backdrop_sizes[0];
+  })
+  .catch(console.log);
+
 class ContentPage extends React.Component {
   constructor(props) {
     super(props);
@@ -10,16 +25,18 @@ class ContentPage extends React.Component {
   }
   handleQueryChange(query) {
     this.setState({ searchQuery: { query } });
+    if (query.trim() === "") {
+      setTimeout(this.setState({ searchResults: [] }), 100000);
+      return;
+    }
     // Should hide this API key later
-    const apiKey = "4b547eeea89c2c56ed31012705fbf0c6";
     const language = "en-US";
     const include_adult = false;
     const page = 1;
     // Also can include an option for year
+    const apiSearchRequest = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=${language}&query=${query}&page=${page}&include_adult=${include_adult}`;
 
-    const apiRequest = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=${language}&query=${query}&page=${page}&include_adult=${include_adult}`;
-
-    fetch(apiRequest)
+    fetch(apiSearchRequest)
       .then((response) => response.json())
       .then((data) => {
         this.setState({ searchResults: { data } });
@@ -57,50 +74,65 @@ class SearchBar extends React.Component {
   }
   render() {
     return (
-      <form>
-        <label htmlFor="searchbar"></label>
-        <input
-          type="text"
-          id="searchbar"
-          placeholder="I'm looking for a movie about..."
-          onChange={this.handleChange}
-        ></input>
-      </form>
+      <div id="searchbar-container">
+        <form>
+          <label htmlFor="searchbar"></label>
+          <input
+            type="text"
+            id="main-searchbar"
+            placeholder="I'm looking for a movie about..."
+            onChange={this.handleChange}
+          ></input>
+        </form>
+      </div>
     );
   }
 }
 
 class ResultsPage extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  // Basically we should have a list of objects after each query
-  // If the ilst is empty, display something particular here
-  // Else just display all the stuff from the movie display, pageifying as necessary
   render() {
     // Pass in the search results from the search bar
     const searchResults = this.props.searchResults;
     const searchQuery = this.props.searchQuery;
-
-    if (searchQuery === "") {
-      return <p></p>;
-    } else if (searchResults.length === 0) {
+    console.log(searchQuery);
+    if (searchQuery.length === 0) {
+      return <p>Hi</p>;
+    } else if (
+      searchResults.length === 0 ||
+      searchResults.data.total_results === 0
+    ) {
+      console.log(searchQuery.length);
+      console.log(typeof searchQuery);
       return <p>Oops, no search results found</p>;
     } else {
-      return <p>Search results</p>;
+      console.log(searchResults);
+      const movieTabs = searchResults.data.results.map(function (movieData) {
+        return <MovieDisplay data={movieData}></MovieDisplay>;
+      });
+      return <div id="results-page">{movieTabs}</div>;
     }
   }
 }
 
 class MovieDisplay extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
   render() {
-    return <div className="movie-display"></div>;
+    const movieData = this.props.data;
+    const img_path = secure_base_url + size + movieData.poster_path;
+    const overview = movieData.overview;
+    const release_date = movieData.release_date;
+    const title = movieData.title;
+    const popularity = movieData.popularity;
+    const vote_average = movieData.vote_average;
+    return (
+      <div className="movie-display-tile">
+        <img src={img_path} alt="Movie Poster"></img>
+        <div id="img-description">
+          <h1>{title}</h1>
+          <p>Average Score: {vote_average}</p>
+          <p>{overview}</p>
+        </div>
+      </div>
+    );
   }
 }
 
