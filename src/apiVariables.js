3c261a3bd99e6e54f780248e4e8d882ec219fcbf
octophi <16705282+Octophi@users.noreply.github.com>
2021-03-05 02:36:26 -0500

    segmented off apiVariables

---
+++ src/apiVariables.js
@@ -0,0 +1,28 @@
+import { config } from "../src/config.js";
+
+const apiKey = config.apiKey;
+
+// Querying for basic API config info, base_url, and size used for finding images
+let secure_base_url, size;
+const apiConfigRequest = `https://api.themoviedb.org/3/configuration?api_key=${apiKey}`;
+fetch(apiConfigRequest)
+  .then((response) => response.json())
+  .then((data) => {
+    secure_base_url = data.images.secure_base_url;
+    size = data.images.backdrop_sizes[0];
+  })
+  .catch(console.log);
+
+// Get mapping of genre ids for en-US, store into genreMapping in (id, name) format
+let genreMapping = new Map();
+const apiGenreRequest = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`;
+fetch(apiGenreRequest)
+  .then((response) => response.json())
+  .then((data) => {
+    data.genres.forEach(function (genrePair) {
+      genreMapping.set(genrePair.id, genrePair.name);
+    });
+  })
+  .catch(console.log);
+
+export { secure_base_url, size, genreMapping };
--- src/index.js
+++ src/index.js
@@ -6,33 +6,11 @@ import MainDescription from "./components/MainDescription";
 import ErrorMessage from "./components/ErrorMessage";
 import PaginationMenu from "./components/PaginationMenu";
 import { config } from "../src/config.js";
+import { secure_base_url, size, genreMapping } from "../src/apiVariables";
 import "./index.css";
 
 // Grab API key, store it for queries
 const apiKey = config.apiKey;
-
-// Querying for basic API config info, base_url, and size used for finding images
-let secure_base_url, size;
-const apiConfigRequest = `https://api.themoviedb.org/3/configuration?api_key=${apiKey}`;
-fetch(apiConfigRequest)
-  .then((response) => response.json())
-  .then((data) => {
-    secure_base_url = data.images.secure_base_url;
-    size = data.images.backdrop_sizes[0];
-  })
-  .catch(console.log);
-
-// Get mapping of genre ids for en-US, store into genreMapping in (id, name) format
-let genreMapping = new Map();
-const apiGenreRequest = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`;
-fetch(apiGenreRequest)
-  .then((response) => response.json())
-  .then((data) => {
-    data.genres.forEach(function (genrePair) {
-      genreMapping.set(genrePair.id, genrePair.name);
-    });
-  })
-  .catch(console.log);
 
 // App component is the entire webpage
 class App extends React.Component {
